c19b1bc9c409baed8802c70b69f3c9fe38a8b23e
Henning Spjelkavik <henning@spjelkavik.net>
2014-12-30 22:36:04 +0100

    Added test cases for configuration of hystrix.

--- src/test/java/no/finn/archaius/ConstrettoConfigSourceTestJunit.java
+++ src/test/java/no/finn/archaius/ConstrettoConfigSourceTestJunit.java
@@ -1,6 +1,8 @@
 package no.finn.archaius;
 
 import com.netflix.config.*;
+import com.netflix.hystrix.HystrixCommand;
+import com.netflix.hystrix.HystrixCommandGroupKey;
 import org.constretto.ConstrettoConfiguration;
 import org.junit.Test;
 
@@ -21,8 +23,30 @@ public class ConstrettoConfigSourceTestJunit {
         DynamicStringProperty myprop = DynamicPropertyFactory.getInstance().getStringProperty("alfa1", "1000");
         String res = myprop.get();
         assertEquals("42", res);
-        System.out.println(res);
+//        System.out.println(res);
 
+    }
+
+    @Test
+    public void testWithHystrix() {
+        TestItCommand tc = new TestItCommand();
+        String result = tc.execute();
+        Integer timeout = tc.getProperties().executionIsolationThreadTimeoutInMilliseconds().get();
+        assertEquals("get the timeout from the config file", 998, timeout.longValue());
+        //System.out.println(timeout);
+//        System.out.println("key: " + tc.getCommandKey().name());
+    }
+
+    static class TestItCommand extends HystrixCommand<String> {
+
+        TestItCommand() {
+            super(HystrixCommandGroupKey.Factory.asKey("TestItGroup"));
+        }
+
+        @Override
+        protected String run() throws Exception {
+            return null;
+        }
     }
 
 }
